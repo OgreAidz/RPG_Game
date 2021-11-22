18a5dc06fee93082600129c0102f4373bb31e070
Lone2866 <boughtflowerc@gmail.com>
2021-11-22 02:50:40 +0000

    Added Player Movement through navigation mesh

---
+++ Assets/Navigation/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+
+    Camera cam;
+
+    PlayerMotor motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Camera").GetComponent<Camera>();
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                Debug.Log("Hit " + hit.collider.name + " " + hit.point);
+                motor.MoveToPoint(hit.point);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                
+            }
+        }
+    }
+}
